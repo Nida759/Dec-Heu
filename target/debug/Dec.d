fb74f47782cfc905a743543d2319c97529fc866d
Your Name <Your Email>
2019-12-12 19:15:53 +0000

    added commit

---
+++ target/debug/Dec.d
@@ -0,0 +1,1 @@
+/home/nida/8_Dec/Dec/target/debug/Dec: /home/nida/8_Dec/Dec/src/main.rs
---
+++ target/debug/deps/Dec-0a91b1495f72b749.d
@@ -0,0 +1,5 @@
+/home/nida/8_Dec/Dec/target/debug/deps/Dec-0a91b1495f72b749: src/main.rs
+
+/home/nida/8_Dec/Dec/target/debug/deps/Dec-0a91b1495f72b749.d: src/main.rs
+
+src/main.rs:
